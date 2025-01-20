6fa2167d150b28e81c09999e5e358f26bf13f4a5
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-01-20 19:43:27 +0800

    feat: OC integration (not tested)

---
+++ src/main/java/gregicality/nuclear/integration/opencomputers/OpenComputersModule.java
@@ -0,0 +1,26 @@
+package gregicality.nuclear.integration.opencomputers;
+
+import gregicality.nuclear.GCYNValues;
+import gregicality.nuclear.integration.opencomputers.drivers.specific.DriverFissionReactor;
+import gregicality.nuclear.modules.GCYNModules;
+import gregtech.api.modules.GregTechModule;
+import gregtech.api.util.Mods;
+import gregtech.integration.IntegrationSubmodule;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import static gregtech.integration.opencomputers.OpenComputersModule.registerDriver;
+
+@GregTechModule(
+        moduleID = GCYNModules.MODULE_OC,
+        containerID = GCYNValues.MODID,
+        modDependencies = Mods.Names.OPEN_COMPUTERS,
+        name = "GCYN OpenComputers Integration",
+        description = "GCYN OpenComputers Integration Module")
+public class OpenComputersModule extends IntegrationSubmodule {
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+        getLogger().info("Registering OpenComputers Drivers...");
+        registerDriver(new DriverFissionReactor());
+    }
+}
--- src/main/java/gregicality/nuclear/modules/GCYNModules.java
+++ src/main/java/gregicality/nuclear/modules/GCYNModules.java
@@ -9,6 +9,7 @@ public class GCYNModules implements IModuleContainer {
 
     public static final String MODULE_CORE = "gcyn_core";
     public static final String MODULE_JEI = "gcyn_jei_integration";
+    public static final String MODULE_OC = "gcyn_oc_integration";
 
     @Override
     public String getID() {
