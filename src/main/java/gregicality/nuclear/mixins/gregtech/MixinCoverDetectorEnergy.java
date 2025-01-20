86d12f6f01ed0b4c65caea1bff8fe6e7a954c4f1
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-01-20 20:24:47 +0800

    feat: custom Energy Detector Cover logic

---
+++ src/main/java/gregicality/nuclear/mixins/gregtech/MixinCoverDetectorEnergy.java
@@ -0,0 +1,36 @@
+package gregicality.nuclear.mixins.gregtech;
+
+import gregicality.nuclear.api.cover.ICustomEnergyCover;
+import gregtech.api.cover.CoverDefinition;
+import gregtech.api.cover.CoverableView;
+import gregtech.common.covers.detector.CoverDetectorBase;
+import gregtech.common.covers.detector.CoverDetectorEnergy;
+import net.minecraft.util.EnumFacing;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = CoverDetectorEnergy.class, remap = false)
+public abstract class MixinCoverDetectorEnergy extends CoverDetectorBase {
+
+    public MixinCoverDetectorEnergy(@NotNull CoverDefinition definition, @NotNull CoverableView coverableView, @NotNull EnumFacing attachedSide) {
+        super(definition, coverableView, attachedSide);
+    }
+
+    @Inject(method = "canAttach", at = @At("HEAD"), cancellable = true)
+    public void iThinkICan(CoverableView coverable, EnumFacing why_should_I_care, CallbackInfoReturnable<Boolean> cir) {
+        if (coverable instanceof ICustomEnergyCover) cir.setReturnValue(true);
+    }
+
+    @Inject(method = "getCoverHolderCapacity", at = @At("HEAD"), cancellable = true)
+    public void getCustomCapacity(CallbackInfoReturnable<Long> cir) {
+        if (getCoverableView() instanceof ICustomEnergyCover custom) cir.setReturnValue(custom.getCoverCapacity());
+    }
+
+    @Inject(method = "getCoverHolderStored", at = @At("HEAD"), cancellable = true)
+    public void getCustomStored(CallbackInfoReturnable<Long> cir) {
+        if (getCoverableView() instanceof ICustomEnergyCover custom) cir.setReturnValue(custom.getCoverStored());
+    }
+}
