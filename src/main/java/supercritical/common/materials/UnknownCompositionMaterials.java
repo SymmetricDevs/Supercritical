03eafbf737f8575ba0dd7c46b726a1a937681b0c
trainvoi <voitrainelephant@gmail.com>
2025-01-30 19:50:39 +0700

    Update UnknownCompositionMaterials.java

--- src/main/java/supercritical/common/materials/UnknownCompositionMaterials.java
+++ src/main/java/supercritical/common/materials/UnknownCompositionMaterials.java
@@ -14,18 +14,12 @@ import gregtech.api.unification.material.Material;
 public class UnknownCompositionMaterials {
 
     public static void register() {
-        Corium = new Material.Builder(1501, scId("corium"))
-                .liquid(new FluidBuilder().temperature(2500).block().density(8.0D).viscosity(10000))
-                .color(0x7A6B50)
-                .iconSet(DULL)
-                .flags(NO_UNIFICATION, STICKY, GLOWING)
-                .build();
 
-        SpentUraniumFuelSolution = new Material.Builder(1502, scId("spent_uranium_fuel_solution"))
+        SpentUraniumFuelSolution = new Material.Builder(1501, scId("spent_uranium_fuel_solution"))
                 .liquid()
                 .color(0x384536).build();
 
-        RadonRichGasMixture = new Material.Builder(1503, scId("radon_rich_gas_mixture"))
+        RadonRichGasMixture = new Material.Builder(1502, scId("radon_rich_gas_mixture"))
                 .gas()
                 .color(0xd78dd9).build();
     }
